9cfb5ba10ee180581c3fb3f4f5d9a915f14f5769
jiyong <wldyd2113@gmail.com>
2025-07-11 15:17:20 +0900

    cell파일 생성

---
+++ 주말과제/SeSACTravel/SeSACTravel/View/Cell/TravelTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TravelTableViewCell.swift
+//  SeSACTravel
+//
+//  Created by 차지용 on 7/11/25.
+//
+
+import UIKit
+
+class TravelTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
