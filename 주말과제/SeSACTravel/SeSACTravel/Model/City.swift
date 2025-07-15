5db180cd5b7e801b25fe2139345512599bcff54e
jiyong <wldyd2113@gmail.com>
2025-07-15 18:50:18 +0900

    city Setting

---
+++ 주말과제/SeSACTravel/SeSACTravel/Model/City.swift
@@ -0,0 +1,16 @@
+//
+//  City.swift
+//  SeSACTravel
+//
+//  Created by 차지용 on 7/15/25.
+//
+
+import Foundation
+
+struct City {
+    let city_name: String?
+    let city_english_name: String?
+    let city_explain: String?
+    let city_image: String?
+    let domestic_travel: Bool?
+}
