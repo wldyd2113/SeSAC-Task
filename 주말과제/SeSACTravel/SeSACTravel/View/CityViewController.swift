be4548ca3cb6a3db63ad23694308590e53726978
jiyong <wldyd2113@gmail.com>
2025-07-16 17:07:47 +0900

    segment

--- 주말과제/SeSACTravel/SeSACTravel/View/CityViewController.swift
+++ 주말과제/SeSACTravel/SeSACTravel/View/CityViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class CityViewController: UIViewController,UITableViewDelegate, UITableViewDataSource {
     
-    
+
     let cityList = CityInfo().city
     
     @IBOutlet var searchTextField: UISearchBar!
@@ -23,21 +23,44 @@ class CityViewController: UIViewController,UITableViewDelegate, UITableViewDataS
         let xib = UINib(nibName: "CityTableViewCell", bundle: nil)
         cityTable.register(xib, forCellReuseIdentifier: "CityTableViewCell")
     }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        return cityList.count
+     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+         
+         return cityList.count
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "CityTableViewCell", for: indexPath) as! CityTableViewCell
-        let cityIndexPath = cityList[indexPath.row]
-        cell.configureCityCell(cityIndexPath: cityIndexPath)
-        return cell
-        
+     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+         let cell = tableView.dequeueReusableCell(withIdentifier: "CityTableViewCell", for: indexPath) as! CityTableViewCell
+         let cityIndexPath = cityList[indexPath.row]
+         if segment.selectedSegmentIndex == 0 {
+             cell.configureCityCell(cityIndexPath: cityIndexPath)
+             tableView.reloadData()
+             print(cityList)
+         }
+         else if segment.selectedSegmentIndex == 1 {
+             if cityIndexPath.domestic_travel == true {
+                 cell.configureCityCell(cityIndexPath: cityIndexPath)
+                 tableView.reloadData()
+
+                 print(cityList)
+
+
+             }
+         }
+         else if segment.selectedSegmentIndex == 2 {
+             if cityIndexPath.domestic_travel == false {
+                 cell.configureCityCell(cityIndexPath: cityIndexPath)
+                 tableView.reloadData()
+
+                 print(cityList)
+             }
+         }
+//         
+         return cell
+
     }
     
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 200
     }
     
@@ -48,9 +71,7 @@ class CityViewController: UIViewController,UITableViewDelegate, UITableViewDataS
         let vc = sb.instantiateViewController(withIdentifier: "CityDetailViewController") as! CityDetailViewController
         vc.listData = cityData
         navigationController?.pushViewController(vc, animated: true)
-    }
-    
-    @IBAction func segmentChanged(_ sender: UISegmentedControl) {
         
     }
+ 
 }
