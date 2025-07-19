29732e9da20a170ee0409020f7fa812bbdf5a1ed
jiyong <wldyd2113@gmail.com>
2025-07-19 20:11:34 +0900

    chat 데이터 연결

--- 주말과제/TravelTalk/TravelTalk/View/Chat/ChatViewController.swift
+++ 주말과제/TravelTalk/TravelTalk/View/Chat/ChatViewController.swift
@@ -7,14 +7,36 @@
 
 import UIKit
 
-class ChatViewController: UIViewController {
-
+class ChatViewController: UIViewController, UITableViewDelegate, UITableViewDataSource  {
+    
+    let chatList = ChatList.list
+     
+    @IBOutlet var chatTableView: UITableView!
+    @IBOutlet var searchTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        let xib = UINib(nibName: "ChattingRoomTableViewCell", bundle: nil)
+        chatTableView.register(xib, forCellReuseIdentifier: "ChattingRoomTableViewCell")
+        
+        chatTableView.dataSource = self
+        chatTableView.delegate = self
     }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        chatList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ChattingRoomTableViewCell", for: indexPath) as! ChattingRoomTableViewCell
+        cell.configureData(chatList[indexPath.row])
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
 
     
 }
