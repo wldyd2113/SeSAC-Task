a1a01279d52e487618f3f841f298712655105a2a
jiyong <wldyd2113@gmail.com>
2025-07-19 18:45:36 +0900

    project Setting

---
+++ 주말과제/TravelTalk/TravelTalk/View/Chat/ChatViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ChatViewController.swift
+//  TravelTalk
+//
+//  Created by 차지용 on 7/19/25.
+//
+
+import UIKit
+
+class ChatViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+}
