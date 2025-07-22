349e88a0aad4b458244bcdc5a3652327e4193427
jiyong <wldyd2113@gmail.com>
2025-07-22 19:10:54 +0900

    packge: SnapKit

---
+++ UpDoungame/UpDoungame/View/UpDownViewController.swift
@@ -0,0 +1,17 @@
+//
+//  UpDownViewController.swift
+//  UpDoungame
+//
+//  Created by 차지용 on 7/22/25.
+//
+
+import UIKit
+
+class UpDownViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
