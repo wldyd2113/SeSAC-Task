6388dd25d5ffc2e96b3beb49b470b4ddbaf27d7a
jiyong <wldyd2113@gmail.com>
2025-07-22 18:55:16 +0900

    init Project

---
+++ UpDoungame/UpDoungame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UpDoungame
+//
+//  Created by 차지용 on 7/22/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
