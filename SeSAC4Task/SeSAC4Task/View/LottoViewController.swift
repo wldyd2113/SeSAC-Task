b28255d584e4d30a817ff2a87b35875074e6a2cb
jiyong <wldyd2113@gmail.com>
2025-07-23 14:20:19 +0900

    Project Setting

---
+++ SeSAC4Task/SeSAC4Task/View/LottoViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LottoViewController.swift
+//  SeSAC4Task
+//
+//  Created by 차지용 on 7/23/25.
+//
+
+import UIKit
+
+class LottoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
---
+++ SeSAC4Task/SeSAC4Task/View/MovieViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MovieViewController.swift
+//  SeSAC4Task
+//
+//  Created by 차지용 on 7/23/25.
+//
+
+import UIKit
+
+class MovieViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
