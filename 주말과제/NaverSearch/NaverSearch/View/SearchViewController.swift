76eef968e043eda27cd3dd6d478d95b16b8cdc96
jiyong <wldyd2113@gmail.com>
2025-07-25 13:29:36 +0900

    NaverSearch Setting

---
+++ 주말과제/NaverSearch/NaverSearch/View/SearchViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SearchViewController.swift
+//  NaverSearch
+//
+//  Created by 차지용 on 7/25/25.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
