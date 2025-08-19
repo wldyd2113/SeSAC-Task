8a15a4c0e502ba4b6fffd8434ebd56b28c25e3df
jiyong <wldyd2113@gmail.com>
2025-08-19 15:34:41 +0900

    init RxProject

---
+++ RxSampleProject/RxSampleProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RxSampleProject
+//
+//  Created by 차지용 on 8/19/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+
+
+}
+
