e6e05a1fc82c4d70abc19be04ac522e675f1bf96
jiyong <wldyd2113@gmail.com>
2025-09-10 18:36:53 +0900

    Design: WeatherUI

--- WeatherMap/WeatherMap/SceneDelegate.swift
+++ WeatherMap/WeatherMap/SceneDelegate.swift
@@ -16,7 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
        
         guard let scene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: scene)
-        let vc = ViewController()
+        let vc = WeatherViewController()
         let nav = UINavigationController(rootViewController: vc)
         window?.rootViewController = nav
         window?.makeKeyAndVisible()
---
+++ WeatherMap/WeatherMap/WeatherViewController.swift
@@ -0,0 +1,115 @@
+//
+//  WeatherViewController.swift
+//  WeatherMap
+//
+//  Created by 차지용 on 9/10/25.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+import SnapKit
+
+class WeatherViewController: ViewController {
+    
+    let mapView = MKMapView()
+    
+    let currentDateLabel = UILabel()
+    let temperatureLabel = UILabel()
+    let humidityLabel = UILabel()
+    let windspeedLabel = UILabel()
+    
+    let currentLocationButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(systemName: "location.fill"), for: .normal)
+        button.layer.cornerRadius = 25
+        button.layer.masksToBounds = false
+        button.backgroundColor = .white
+        
+        button.layer.shadowColor = UIColor.black.cgColor
+        button.layer.shadowOpacity = 0.5
+        button.layer.shadowOffset = CGSize(width: 0, height: 4)
+        button.layer.shadowRadius = 4
+        return button
+    }()
+    
+    let resetButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(systemName: "arrow.clockwise"), for: .normal)
+        button.layer.cornerRadius = 25
+        button.layer.masksToBounds = false
+        button.backgroundColor = .white
+        
+        button.layer.shadowColor = UIColor.black.cgColor
+        button.layer.shadowOpacity = 0.5
+        button.layer.shadowOffset = CGSize(width: 0, height: 4)
+        button.layer.shadowRadius = 4
+        return button
+    }()
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configure()
+        configureUI()
+        configureLayout()
+       
+    }
+    
+    func configure() {
+        view.addSubview(mapView)
+        view.addSubview(currentDateLabel)
+        view.addSubview(temperatureLabel)
+        view.addSubview(windspeedLabel)
+        view.addSubview(humidityLabel)
+        view.addSubview(currentLocationButton)
+        view.addSubview(resetButton)
+    }
+    
+    func configureUI() {
+        view.backgroundColor = .white
+        currentDateLabel.text = "날씨정보를 불러오는중..."
+        temperatureLabel.text = "현재온도"
+        windspeedLabel.text = "풍속"
+        humidityLabel.text = "습도"
+    }
+    
+    func configureLayout() {
+        mapView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide)
+            make.leading.trailing.equalToSuperview()
+            make.height.equalTo(350)
+            
+        }
+        currentDateLabel.snp.makeConstraints { make in
+            make.top.equalTo(mapView.snp.bottom).offset(15)
+            make.centerX.equalToSuperview()
+        }
+        temperatureLabel.snp.makeConstraints { make in
+            make.top.equalTo(currentDateLabel.snp.bottom)
+            make.centerX.equalToSuperview()
+        }
+        windspeedLabel.snp.makeConstraints { make in
+            make.top.equalTo(temperatureLabel.snp.bottom)
+            make.centerX.equalToSuperview()
+        }
+        humidityLabel.snp.makeConstraints { make in
+            make.top.equalTo(windspeedLabel.snp.bottom)
+            make.centerX.equalToSuperview()
+        }
+        currentLocationButton.snp.makeConstraints { make in
+            make.bottom.equalTo(view.safeAreaLayoutGuide).inset(20)
+            make.leading.equalToSuperview().inset(20)
+            make.height.width.equalTo(50)
+        }
+        
+        resetButton.snp.makeConstraints { make in
+            make.bottom.equalTo(view.safeAreaLayoutGuide).inset(20)
+            make.trailing.equalToSuperview().inset(20)
+            make.height.width.equalTo(50)
+        }
+
+    }
+    
+
+}
